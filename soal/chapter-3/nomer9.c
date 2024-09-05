2af26daaf02f866dcbec4e3bd287daaf9b00dcf6
Hamzah Ali <abdillahhamzahali@gmail.com>
2024-09-05 20:20:19 +0700

    finis

---
+++ soal/chapter-3/nomer9.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+void predict_formula();
+
+int main(void)
+{
+    int day;
+    printf("FLU EPIDEMIC PREDICTIONS BASED ON ELAPSED DAYS SINCE FIRST CASE REPORT \n");
+
+    predict_formula();
+    predict_formula();
+    predict_formula();
+
+    return 0;
+}
+
+void predict_formula()
+{
+    // input
+    int day;
+    printf("Enter day number>> ");
+    scanf("%d", &day);
+
+    // compute
+    int i = 40000 / (1 + 39999 * exp(-0.24681 * day));
+    // print output
+
+    printf("By day %d, model predicts %d cases total.\n", day, i);
+}
