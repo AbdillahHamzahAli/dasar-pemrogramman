a5ba9dc2fb084b6c309349394309f1dd049cf8ad
Hamzah Ali <abdillahhamzahali@gmail.com>
2024-09-29 23:13:23 +0700

    add : yang penting selesai

---
+++ soal/chapter-5/16.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+double next_approx(double, double, double);
+double func(double, double, double);
+double derivative(double, double);
+
+int main()
+{
+	double x0, n, c, x1, i, temp;
+	printf("\nEnter c - ");
+	scanf("%lf", &c);
+	printf("\nEnter n - ");
+	scanf("%lf", &n);
+	
+	x0 = c / 2;
+	x1 = x0 - (func(x0, n, c) / derivative(x0, n));
+	
+	for(i = 0; i < 100; i++)
+	{
+		printf("\n%.6f \n", x1);
+		x1 = next_approx(x1, n, c);
+	}
+    return 0;
+}
+
+double next_approx(double temp, double n, double c)
+{
+	double next_approx = temp - (func(temp, n, c) / derivative(temp, n));
+	return next_approx;
+}
+
+double func(double x, double n, double c)
+{
+	return ((pow(x, n)) - c);
+}
+
+double derivative(double x, double n)
+{
+	return (x * pow(x, n - 1));
+}
